d1f8172b412a058519d074bf1122f0c8bc613840
dbobe <bobe.david@live.com>
2024-07-02 17:19:46 -0400

    Banner component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100...900&display=swap"
           rel="stylesheet"
         />
       </Head>
